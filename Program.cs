117ec44ddf4de0140505a338151df86a094727d1
DimasVeliz <trevor90094@gmail.com>
2021-06-09 22:05:30 +0200

    Scanner created

--- Program.cs
+++ Program.cs
@@ -6,7 +6,8 @@ namespace FormulaEngine
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var expression ="1+2";
+            System.Console.WriteLine(expression);
         }
     }
 }
---
+++ SourceScanner.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace FormulaEngine
+{
+    class SourceScanner
+    {
+        readonly string _buffer;
+    }
+}
