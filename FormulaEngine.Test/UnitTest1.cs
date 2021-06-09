ce1393670c5b95fa860f3fde64bbe10896854e7a
DimasVeliz <trevor90094@gmail.com>
2021-06-09 23:46:34 +0200

    Test passed

--- FormulaEngine.Test/UnitTest1.cs
+++ FormulaEngine.Test/UnitTest1.cs
@@ -8,7 +8,26 @@ namespace FormulaEngine.Test
         [Fact]
         public void Test1()
         {
+            var expression = "1 + 2";
+            (TokenType,int,string)[] expectedResults = new (TokenType,int,string)[]{
+                (TokenType.Number,0,"1"),
+                (TokenType.Addition,2,"+"),
+                (TokenType.Number,4,"2")
 
+            };
+
+            var lexer = new Lexer(new SourceScanner(expression));
+
+            foreach (var (t,p,v) in expectedResults)
+            {
+                var token = lexer.ReadNext();
+                Assert.Equal(t,token.Type);
+                Assert.Equal(p,token.Position);
+                Assert.Equal(v,token.Value);
+
+            }
+
+            Assert.Equal(TokenType.EOE,lexer.ReadNext().Type);
         }
     }
 }
