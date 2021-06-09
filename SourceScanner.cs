f9f6b54051461574584864bda0a96b11929b35e6
DimasVeliz <trevor90094@gmail.com>
2021-06-09 22:14:13 +0200

    Scanner completed

--- SourceScanner.cs
+++ SourceScanner.cs
@@ -1,9 +1,41 @@
 using System;
+using System.Collections.Generic;
 
 namespace FormulaEngine
 {
     class SourceScanner
     {
+        readonly Stack<int> PositionStack = new Stack<int>();
         readonly string _buffer;
+        public int Position { get; private set; }
+
+        public bool EndOfSource => Position >= _buffer.Length; 
+
+        
+        public char? Read()
+        {
+            if (EndOfSource)
+            {
+                return (char?)null;
+            }
+            return _buffer[Position++];
+        }
+
+        public char? Peek()
+        {
+            Push();
+            var next = Read();
+            Pop();
+            return next;
+        }
+
+        public void Push()
+        {
+            PositionStack.Push(Position);
+        }
+        public void Pop()
+        {
+            Position = PositionStack.Pop();
+        }
     }
 }
