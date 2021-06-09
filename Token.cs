f3ac01e9f29c8fa38656182d3e8dc530871b08ea
DimasVeliz <trevor90094@gmail.com>
2021-06-09 22:21:22 +0200

    Token class body implemented

--- Token.cs
+++ Token.cs
@@ -3,7 +3,26 @@ using System.Collections.Generic;
 
 namespace FormulaEngine
 {
-    class Token
+    public enum TokenType
+        {
+            EOE,
+            Addition,
+            Substraction,
+            Multiplication,
+            Division
+        }
+
+    public class Token
     {
+        public int Position {get;}
+        public TokenType Type {get;}
+        public string Value {get;}
+
+        public Token(TokenType type, int position, string value)
+        {
+            Type = type;
+            Position = position;
+            Value = value;
+        }
     }
 }
