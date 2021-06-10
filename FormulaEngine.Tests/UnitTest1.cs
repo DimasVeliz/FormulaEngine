93c566a719d119c5dd7d88c84dedfcab7c45e786
DimasVeliz <trevor90094@gmail.com>
2021-06-10 17:34:25 +0200

    Parser Succesfully tested

--- FormulaEngine.Tests/UnitTest1.cs
+++ FormulaEngine.Tests/UnitTest1.cs
@@ -4,10 +4,11 @@ using Xunit;
 
 namespace FormulaEngine.Tests
 {
+
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void LexerTest()
         {
             var expression = "1 + 2";
             (TokenType, int, string)[] expectedResults = new (TokenType, int, string)[]{
@@ -29,6 +30,57 @@ namespace FormulaEngine.Tests
             }
 
             Assert.Equal(TokenType.EOE, lexer.ReadNext().Type);
+        }
+
+
+        [Fact]
+        public void ParseTest()
+        {
+            var expression = "1 + 2";
+
+            var ast = (BinaryOperatorASTNode)Parser.Parse(expression);
+            Assert.NotNull(ast);
+
+            Assert.Equal(TokenType.Addition, ast.Token.Type);
+            Assert.Equal(TokenType.Number, ast.Left.Token.Type);
+            Assert.Equal(TokenType.Number, ast.Right.Token.Type);
+
+            Assert.Equal(1, (ast.Left as NumberASTNode).Value);
+            Assert.Equal(2, (ast.Right as NumberASTNode).Value);
+
+        }
+
+        [Fact]
+        public void ParseTest02()
+        {
+            var expression = "1 + 2*3";
+
+            var ast = (BinaryOperatorASTNode)Parser.Parse(expression);
+
+            Assert.NotNull(ast);
+
+            Assert.Equal(TokenType.Addition, ast.Token.Type);
+            Assert.Equal(TokenType.Number, ast.Left.Token.Type);
+
+            Assert.Equal(TokenType.Multiplication, ast.Right.Token.Type);
+            Assert.Equal(TokenType.Number, (ast.Right as BinaryOperatorASTNode).Left.Token.Type);
+            Assert.Equal(TokenType.Number, (ast.Right as BinaryOperatorASTNode).Right.Token.Type);
+
+
+            Assert.Equal(1, (ast.Left as NumberASTNode).Value);
+            Assert.Equal(2, ((ast.Right as BinaryOperatorASTNode).Left as NumberASTNode).Value);
+            Assert.Equal(3, ((ast.Right as BinaryOperatorASTNode).Right as NumberASTNode).Value);
+
+        }
+        [Fact]        
+        public void ParserTest03()
+        {
+            var expression = "1 +";
+
+           
+
+            Assert.Throws<Exception>(()=>Parser.Parse(expression));
+
         }
     }
 }
