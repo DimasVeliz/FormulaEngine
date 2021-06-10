9da285a2614c71873ba0befc94c15b094885733d
DimasVeliz <trevor90094@gmail.com>
2021-06-10 12:41:38 +0200

    ASTNode classes added and test build folder ignored

---
+++ FormulaEngine.Logic/ASTNode.cs
@@ -0,0 +1,75 @@
+
+namespace FormulaEngine.Logic
+{
+    //this class implements the following matching rules
+    // OPERATOR L + | - | * | /
+    // NUMBER: [0-9]+
+
+    public abstract class ASTNode
+    {
+        public Token Token { get; set; }
+        public ASTNode(Token token)
+        {
+            Token = token;
+        }
+
+    }
+
+    public class NumberASTNode : ASTNode
+    {
+        public int Value => int.Parse(Token.Value);
+        public NumberASTNode(Token token) : base(token)
+        {
+        }
+
+    }
+
+    public abstract class OperatorASTNode : ASTNode
+    {
+        protected OperatorASTNode(Token token) : base(token)
+        {
+        }
+    }
+
+    public abstract class BinaryOperatorASTNode : OperatorASTNode
+    {
+        
+        public ASTNode Left { get; }
+        public ASTNode Right { get; }
+        public BinaryOperatorASTNode(Token token, ASTNode left, ASTNode right) : base(token)
+        {
+            Token = token;
+            Left = left;
+            Right = right;
+        }
+
+    }
+
+    public class AdditionBinaryOperatorASTNode : BinaryOperatorASTNode
+    {
+        public AdditionBinaryOperatorASTNode(Token token, ASTNode left, ASTNode right) : base(token, left, right)
+        {
+        }
+    }
+
+    public class SubstractionBinaryOperatorASTNode : BinaryOperatorASTNode
+    {
+        public SubstractionBinaryOperatorASTNode(Token token, ASTNode left, ASTNode right) : base(token, left, right)
+        {
+        }
+    }
+
+    public class MultiplicationBinaryOperatorASTNode : BinaryOperatorASTNode
+    {
+        public MultiplicationBinaryOperatorASTNode(Token token, ASTNode left, ASTNode right) : base(token, left, right)
+        {
+        }
+    }
+
+    public class DivisionBinaryOperatorASTNode : BinaryOperatorASTNode
+    {
+        public DivisionBinaryOperatorASTNode(Token token, ASTNode left, ASTNode right) : base(token, left, right)
+        {
+        }
+    }
+}
