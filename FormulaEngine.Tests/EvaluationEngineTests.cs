7177808c83622bcef64e4f11a58c3840b51e0e89
DimasVeliz <trevor90094@yandex.com>
2021-06-15 00:52:46 +0200

    Evaluation Texted with Floating point numbers

--- FormulaEngine.Tests/EvaluationEngineTests.cs
+++ FormulaEngine.Tests/EvaluationEngineTests.cs
@@ -19,5 +19,17 @@ namespace FormulaEngine.Tests
             //Then
             Assert.Equal(7, result);
         }
+
+        [Fact]
+        public void EvaluationTestFP001()
+        {
+             var expression = "1 + 1e5";
+            //When
+            var result = EvaluationEngine.Evaluate(expression);
+
+
+            //Then
+            Assert.Equal(100001, result);
+        }
     }
 }
